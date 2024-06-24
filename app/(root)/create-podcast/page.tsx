9c9cbf503d6c19324080c2f37583cd1dc34a2ba0
Sahaab-Tariq <sahabtariq888@gmail.com>
2024-06-24 11:57:24 +0500

    generate thumbnail

--- app/(root)/create-podcast/page.tsx
+++ app/(root)/create-podcast/page.tsx
@@ -177,7 +177,7 @@ const CreatePodcast = () => {
                 setVoicePrompt={setVoicePrompt}
                 setAudioDuration={setAudioDuration}
               />
-
+              <GenerateThumbnail />
               <div className="mt-10 w-full">
                 <Button type="submit" className="text-16 w-full bg-orange-1 py-4 font-extrabold text-white-1 transition-all duration-500 hover:bg-black-1">
                   {isSubmitting ? (
